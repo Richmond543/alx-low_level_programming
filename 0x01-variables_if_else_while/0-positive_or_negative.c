194d110530fb828d8f2462ec874d10e69bef4cc6
Richmond543 <70352481+Richmond543@users.noreply.github.com>
2022-09-12 13:18:20 -0700

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -21,13 +21,11 @@ printf("%i is positive\n", n);
 }
 else if (n == 0)
 {
-printf("%i is zero\n",n);
+printf("%i is zero\n", n);
 }
 else if (n < 0)
 {
 printf("%i is negative\n", n);
 }
-return(0);
+return (0);
 }
-  
-  
